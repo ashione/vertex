e379189c9e8372b596573cdcd3a5c203458cc7a8
ashione <skyzlxuan@gmail.com>
2025-08-11 00:33:54 +0800

    lint fix

--- vertex_flow/workflow/vertex/llm_vertex.py
+++ vertex_flow/workflow/vertex/llm_vertex.py
@@ -134,7 +134,6 @@ class LLMVertex(Vertex[T]):
             if not (all_inputs and CONVERSATION_HISTORY in all_inputs):
                 self.messages = []
 
-
             # 获取 task 函数的签名
             sig = inspect.signature(self._task)
             has_context = "context" in sig.parameters
